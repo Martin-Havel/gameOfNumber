33b89aad07dc9979518d8c4b9c257f115ea1dc4e
Martin-Havel <it-martinhavel@stredniskola.net>
2024-06-10 09:46:24 +0200

    Luky

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include "iostream"
 #include "color.h"
-#include "ctime"
  /*Hadani tajneho cisla*/
 using namespace std;
  
 int main(int argc, char *argv[]){
     system("chcp 65001>01");
-    srand(time(0));
  int tajneCislo;
  int hracovoCislo;
  tajneCislo = rand ()%5+1; //nahodne cislo do 1 do 5
- 
  cout << "Zadej svuj typ na tajne cislo: ";
  cin >> hracovoCislo;
- 
+ if(tajneCislo == hracovoCislo) {
+    cout << "Správně!" << endl;
+ } else cout << "Neuhodl!" << endl;
 return 0;
 }
