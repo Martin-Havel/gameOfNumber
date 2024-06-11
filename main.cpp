df883c54abec99b96246e5acb0f80a4b6dc10bde
Martin-Havel <it-martinhavel@stredniskola.net>
2024-06-11 08:11:57 +0200

    hotovo

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]){
  int tajneCislo;
  int hracovoCislo;
  tajneCislo = rand ()%5+1; //nahodne cislo do 1 do 5
- cout << "Zadej svuj typ na tajne cislo: ";
+ cout << "Zadej svuj typ na tajne cislo: ";//loterie
  cin >> hracovoCislo;
  if(tajneCislo == hracovoCislo) {
     cout << "Správně!" << endl;
